6e509051d55af9adab2dfbf0ee06983d4b89671f
Pratyush <pratyush041002@gmail.com>
2025-03-01 22:01:45 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function App() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h2>This is a test</h2>
+            <p>This is a paragraph</p>
+        </div>
+    )
+}
+
+export default App ; 
+
+let a = 234 ; 
+let b = 324 ; 
+console.log(a+b) ;
+console.log(a*b) ; 
+console.log(a/b) ; 
+console.log(a-b) ; 
